0865552360b2bc24578d5fd46b104947cdb06bc5
Nguyen Nguyen Vu(Masshiro) <vunngcs220106@fpt.edu.vn>
2024-09-27 22:01:04 +0700

    Feat(config): Implement env var configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { classes } from "@automapper/classes"
 import { AutomapperModule } from "@automapper/nestjs"
 import { Module } from "@nestjs/common"
+import { ConfigModule } from "@nestjs/config"
+import { validate } from "libs/config"
 import { UserModule } from "./users/application/user.module"
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ validate }),
     AutomapperModule.forRoot({
       strategyInitializer: classes(),
     }),
--- src/users/infrastructure/database/prisma/prisma.service.ts
+++ src/users/infrastructure/database/prisma/prisma.service.ts
@@ -1,8 +1,19 @@
 import { Injectable, type OnModuleInit } from "@nestjs/common"
 import { PrismaClient } from "@prisma/client"
+import config from "libs/config"
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    const databaseUrl = `postgresql://${config.DATABASE_USERNAME}:${config.DATABASE_PASSWORD}@${config.DATABASE_HOST}:${config.DATABASE_PORT}/${config.DATABASE_NAME}?schema=${config.DATABASE_SCHEMA}`
+    super({
+      datasources: {
+        db: {
+          url: databaseUrl,
+        },
+      },
+    })
+  }
   async onModuleInit() {
     await this.$connect()
   }
