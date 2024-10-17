08926f0686a63e090b8295a3dbf2cadf94a23af4
Nguyen Nguyen Vu(Masshiro) <vunngcs220106@fpt.edu.vn>
2024-10-17 21:21:52 +0700

    Feat(permission): Implement permission guard

---
+++ src/guard/permission.guard.ts
@@ -0,0 +1,67 @@
+import {
+  BadRequestException,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+} from "@nestjs/common"
+import { Reflector } from "@nestjs/core"
+import { Request } from "express"
+import { PERMISSION_KEY } from "libs/decorator/permission.decorator"
+import { PresentationErrorDetailCode } from "libs/exception/presentation"
+import { Observable } from "rxjs"
+import { Permission } from "src/module/users/domain/entity/permissions.entity"
+import { IUserRepository } from "src/module/users/domain/repository/user/user.interface.repository"
+
+@Injectable()
+export class PermissionGuard implements CanActivate {
+  constructor(
+    private readonly reflector: Reflector,
+    private readonly userRepository: IUserRepository,
+  ) {}
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request: Request = context.switchToHttp().getRequest()
+    try {
+      if (!request.user) {
+        throw new BadRequestException(
+          "Unauthorized",
+          JSON.stringify({
+            info: { errorCode: PresentationErrorDetailCode.UNAUTHORIZED },
+          }),
+        )
+      }
+      const permissions = this.reflector.getAllAndOverride<string[]>(
+        PERMISSION_KEY,
+        [context.getHandler(), context.getClass()],
+      )
+      if (!permissions) {
+        return true
+      }
+      const userPermissions = await this.userRepository.getUserPermissions(
+        request.user,
+      )
+      for (const permission of permissions) {
+        const userPermission = userPermissions.find(
+          (e) => e.name === permission,
+        )
+        if (!userPermission) {
+          throw new BadRequestException(
+            "You do not have permission to access this resource",
+            JSON.stringify({
+              info: { errorCode: PresentationErrorDetailCode.UNAUTHORIZED },
+            }),
+          )
+        }
+      }
+    } catch (err) {
+      throw new BadRequestException(
+        err.message,
+        JSON.stringify({
+          info: { errorCode: PresentationErrorDetailCode.UNAUTHORIZED },
+        }),
+      )
+    }
+
+    return true
+  }
+}
