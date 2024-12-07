f95e2215f4938c1126687b1372ff12a77f71887b
Nguyen Nguyen Vu(Masshiro) <vunngcs220106@fpt.edu.vn>
2024-12-08 03:29:34 +0700

    Feat(lib): modify libs

--- libs/constants/interface.ts
+++ libs/constants/interface.ts
@@ -1,5 +1,11 @@
+import { Socket } from "socket.io"
+import { UserAggregate } from "src/module/users/domain/aggregate"
+
 export interface Response<T> {
   statusCode: number
   message: string
   data: T
+}
+export interface AuthenticatedSocket extends Socket {
+  user?: UserAggregate
 }
--- libs/decorator/decoded_token_payload.decorator.ts
+++ libs/decorator/decoded_token_payload.decorator.ts
@@ -1,7 +1,9 @@
 import { createParamDecorator, ExecutionContext } from "@nestjs/common"
 import { Request } from "express"
 
-export const DecodedTokenPayload = createParamDecorator((_, ctx: ExecutionContext) => {
-  const req: Request = ctx.switchToHttp().getRequest()
-  return req.decoded_access_token
-})
+export const DecodedTokenPayload = createParamDecorator(
+  (_, ctx: ExecutionContext) => {
+    const req: Request = ctx.switchToHttp().getRequest()
+    return req.decoded_access_token
+  },
+)
